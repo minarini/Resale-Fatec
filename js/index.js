9b5f5056fb7c91d5e043a9c1504faf03ea61760e
minarini <nicollerpimenta@gmail.com>
2024-06-15 15:59:19 -0300

    update1506

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-function autenticar(){
-    let nome = document.getElementById('nome').value
-    let dataNascimento = document.getElementById('data_nascimento').value
-    let cpf = document.getElementById('cpf').value
-    let email = document.getElementById('email').value
-    let senha = document.getElementById('senha').value
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede a submissão inicial do formulário
 
-    if (email==''){
-        alert('Email está vazio')
-    }else if(senha==''){
-        alert ('Senha esta vazia')
-    }else if (cpf==''){
-        alert ('cpf está vazia')
-    }else if (nome==''){
-        alert('email está vazio')
-    }else {
-        alert ('Voce foi autenticado')
+    const dataNascimento = document.getElementById('inp_data_nascimento').value;
+    const hoje = new Date();
+    const dataNasc = new Date(dataNascimento);
+
+    const idade = hoje.getFullYear() - dataNasc.getFullYear();
+    const mes = hoje.getMonth() - dataNasc.getMonth();
+    const dia = hoje.getDate() - dataNasc.getDate();
+
+    if (idade < 18 || (idade === 18 && (mes < 0 || (mes === 0 && dia < 0)))) {
+        alert('Você deve ter mais de 18 anos para se cadastrar.');
+    } else {
+        alert('Cadastrado com sucesso!');
+        setTimeout(function() {
+            event.target.submit(); // Submete o formulário após exibir a mensagem
+        }, 100); // Ajuste o tempo de acordo com a necessidade
     }
-
-}
+});
